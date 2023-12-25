262737044958bc7d51d41c7e27befbb466b0ffa3
rajdeepGolokaIT <rajdeep@golokait.com>
2023-12-25 16:19:03 +0530

    homepage structure(need to change the contents)

--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,151 @@
 export default function Home() {
   return (
     <div>
-      
+    <nav >
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Browse Fundraisers</a></li>
+        <li>
+          <a>Fundraisers for</a>
+          <ul className="p-2">
+            <li><a>Medical Treatment</a></li>
+            <li><a>NGO/Charity</a></li>
+            <li><a>Other Cause</a></li>
+          </ul>
+        </li>
+        <li><a>How It Works</a></li>
+      </ul>
     </div>
+    <a className="btn btn-ghost text-xl">Logo</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Browse Fundraisers</a></li>
+      <li>
+        <details>
+          <summary>Fundraisers for</summary>
+          <ul className="p-2">
+          <li><a>Medical Treatment</a></li>
+            <li><a>NGO/Charity</a></li>
+            <li><a>Other Cause</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>How It Works</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end px-5">
+  <input type="text" placeholder="Search" className="input input-ghost w-full max-w-xs mx-5" />
+  <button className="btn btn-outline btn-default mx-5">Chat</button>
+    <a className="btn">Sign In</a>
+  </div>
+</div>
+</nav>
+<div className="carousel carousel-center max-w-full p-4 my-40 space-x-4 bg-neutral rounded-box">
+  
+  <div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> 
+<div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> 
+<div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> 
+<div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+<div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> 
+<div className="carousel-item card w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <progress className="progress progress-success w-full" value="70" max="100"></progress>
+    <div className="card-actions justify-between">
+    <button className="btn btn-outline">Share</button>
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+</div>
+<div className="mockup-window border mx-auto bg-base-300 w-[1000px]">
+<div className="card flex-row w-full bg-base-100 shadow-xl">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body flex flex-row">
+    <div>
+    <h2 className="card-title">Want to Donate now?</h2>
+    <p>donation story</p></div>
+    <div className="card-actions justify-end mt-72">
+      <button className="btn btn-primary">Donate Now</button>
+    </div>
+  </div>
+</div>
+</div>
+</div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  daisyui: {
+    themes: [
+      "retro",
+    ],
+  },
   plugins: [require('daisyui')],
 };
