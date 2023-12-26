73052a64d69f24b0005fc8f8a4a8d0708ba29b56
gayatri-golokait <gayatri@golokait.com>
2023-12-26 18:09:38 +0530

    Mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
 dotenv.config()
 
 const app = express();
@@ -7,9 +8,20 @@ app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 
+//MongoDb connection
+
+const mongoDb = ()=>{
+    const connect = mongoose.connect(process.env.MONGODBURL)
+    if(connect){
+        console.log("mongodb connection")
+    }
+}
+
+
 
 app.listen(PORT, ()=>{
     console.log(`port running on ${PORT} `)
+    mongoDb()
 })
 
 
