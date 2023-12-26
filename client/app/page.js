d81a770d1d409b311d4d817a6109fdeaf138494f
rajdeepGolokaIT <rajdeep@golokait.com>
2023-12-26 15:56:19 +0530

    26/12/23(part-1)

--- client/app/page.js
+++ client/app/page.js
@@ -1,313 +1,99 @@
+
 export default function Home() {
   return (
     <div>
-      <nav>
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost lg:hidden"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a>Browse Fundraisers</a>
-                </li>
-                <li>
-                  <a>Fundraisers for</a>
-                  <ul className="p-2">
-                    <li>
-                      <a>Medical Treatment</a>
-                    </li>
-                    <li>
-                      <a>NGO/Charity</a>
-                    </li>
-                    <li>
-                      <a>Other Cause</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a>How It Works</a>
-                </li>
-              </ul>
-            </div>
-            <a className="btn btn-ghost text-xl">Logo</a>
-          </div>
-          <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-              <li>
-                <a>Browse Fundraisers</a>
-              </li>
-              <li>
-                <details>
-                  <summary>Fundraisers for</summary>
-                  <ul className="p-2">
-                    <li>
-                      <a>Medical Treatment</a>
-                    </li>
-                    <li>
-                      <a>NGO/Charity</a>
-                    </li>
-                    <li>
-                      <a>Other Cause</a>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <a>How It Works</a>
-              </li>
-            </ul>
-          </div>
-          <div className="navbar-end px-5">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-ghost w-full max-w-xs mx-5"
-            />
-            <button className="btn btn-outline btn-default mx-5">Chat</button>
-            <a className="btn">Sign In</a>
-          </div>
-        </div>
-      </nav>
-      <div className="carousel carousel-center max-w-full p-4 my-40 space-x-4 bg-neutral rounded-box">
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="carousel-item card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <progress
-              className="progress progress-success w-full"
-              value="70"
-              max="100"
-            ></progress>
-            <div className="card-actions justify-between">
-              <button className="btn btn-outline">Share</button>
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
+      {/* navbar */}
+      <div className="navbar bg-base-100 w-[80%] mx-auto shadow-2xl shadow-pink-400 rounded-xl mt-5">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <div className="mockup-window border mx-auto bg-base-300 w-[1000px]">
-        <div className="card flex-row w-full bg-base-100 shadow-xl">
-          <figure>
-            <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body flex flex-row">
-            <div>
-              <h2 className="card-title">Want to Donate now?</h2>
-              <p>donation story</p>
-            </div>
-            <div className="card-actions justify-end mt-72">
-              <button className="btn btn-primary">Donate Now</button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-row mx-14">
-        <figure>
-          <img
-            src="https://ketto.gumlet.io/assets/images/homepage/sac-separator.png?w=100&dpr=1.3"
-            alt=""
-            height={450}
-            width={64}
-          />
-        </figure>
-        <div className="md:col-span-10 w-1/4">
-  <div className="flow-wrapper pt-20 flex flex-col justify-between">
-    <div className="start-fr">
-      <span className="text-xl font-bold sub-heading">
-        Start your fundraiser
-      </span>
-      <span className="text-gray-700 descr">
-        It'll take only 2 minutes. Just tell us a few details about you and the ones you are raising funds for.
-      </span>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Home</a></li>
+        <li><a>Our Mission</a></li>
+        <li><a>About The Project</a></li>
+        <li>
+          <a>Become a Patron</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Contact Us</a></li>
+      </ul>
     </div>
-    <div className="share-fr">
-      <span className="text-xl font-bold sub-heading">
-        Share your fundraiser
-      </span>
-      <span className="text-gray-700 descr">
-        All you need to do is share the fundraiser with your friends and family. In no time, support will start pouring in.
-      </span>
-      <span className="text-gray-700 sub-desc">
-        Share your fundraiser directly from the dashboard on social media.
-      </span>
+    <a className="px-2"><img src='/Iskcon-logo.png' alt="logo" width={62} height={60} /></a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+        <li><a>Home</a></li>
+        <li><a>Our Mission</a></li>
+        <li><a>About The Project</a></li>
+      <li>
+        <details>
+          <summary>Become a Patron</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Contact Us</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+  <button className="btn btn-ghost btn-circle">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+    </button>
+  </div>
+</div>
+
+{/* carousal */}
+
+<div className="carousel w-[80%] mt-28 mx-[10%] rounded-xl">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="/Carousal-1.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
-    <div className="withdrw-funds">
-      <span className="text-xl font-bold sub-heading">
-        Withdraw Funds
-      </span>
-      <span className="text-gray-700 descr">
-        The funds raised can be withdrawn without any hassle directly to your bank account.
-      </span>
-      <span className="text-gray-700 sub-desc">
-        It takes only 5 minutes to withdraw funds on ketto.
-      </span>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="/Carousal-2.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="/Carousal-3.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="/Carousal-4.jpg" className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
     </div>
   </div>
 </div>
-<div className="mockup-phone border-primary">
-  <div className="camera"></div> 
-  <div className="display">
-    <div className="artboard artboard-demo phone-1">Hi.</div>
+
+{/* about us */}
+<div className="w-[80%] mx-[10%] mt-28">
+  <h1 className="text-3xl font-bold text-center mb-12 ">ABOUT US</h1>
+  <div className="flex space-x-20 justify-center">
+  <a href="#"><img src="/mission-ic.png" className="w-32"/></a>
+  <a href="#"><img src="/msg-cm.png" className="w-32"/></a>
+  <a href="#"><img src="/testimonial-ic.png" className="w-32"/></a>
+  <a href="#"><img src="/about-prjct.png" className="w-32"/></a>
+  <a href="#"><img src="/team-ic.png" className="w-32"/></a>
+  <a href="#"><img src="/curr-prjct.png" className="w-32"/></a>
   </div>
 </div>
-      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,8 +3,36 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   daisyui: {
     themes: [
-      "pastel",
+      {
+        mytheme: {
+        
+"primary": "#ffe4e6",
+        
+"secondary": "#fecdd3",
+        
+"accent": "#ffdce6",
+        
+"neutral": "#ffffff",
+        
+"base-100": "#ffffff",
+        
+"info": "#ffe4e6",
+        
+"success": "#ffdce6",
+        
+"warning": "#fda4af",
+        
+"error": "#fb7185",
+        },
+      },
     ],
+  },
+  theme: {
+    extend: {
+      backgroundImage: {
+        hareKrishna: 'url:("/hare-krishna-bg.png")',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
