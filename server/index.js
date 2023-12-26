1c1e989f80da0097b331b4ce186704d702ccf21b
gayatri-golokait <gayatri@golokait.com>
2023-12-26 17:18:09 +0530

    basic server setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+app.use(express.json())
+const PORT = process.env.PORT || 5000
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`port running on ${PORT} `)
+})
+
+
+
+
